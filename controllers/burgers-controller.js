fe97c516664d99aa2e59db75b0815e98e2a8de01
91integ25 <91integ25@gmail.com>
2017-05-08 19:08:01 -0400

    heroku push 2

--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -10,14 +10,6 @@ router.get("/",function(req,res){
 });
 
 router.put("/:id",function(req,res){
-
-
-	// burger.update({
-	// 	devoured: req.body.devoured
-	// },condition,function(){
-	// 	res.redirect("/")
-	// });
-	console.log(req.body);
 	db.burger.update({devoured:req.body.devoured},
 		{where:{
 		id:req.params.id
@@ -26,16 +18,6 @@ router.put("/:id",function(req,res){
 	})
 })
 router.post("/",function(req,res){
-	// if(req.body.burger_name === ""){
-	// 	res.send("Name a burger you would like to eat");
-	// }else{
-	// 		burger.create([
-	// 	"burger_name","devoured"],
-	// 	[req.body.burger_name,req.body.devoured],
-	// 	function(){
-	// 		res.redirect("/");
-	// 	})
-	// }
 	db.burger.create(req.body).then(function(){
 		res.redirect("/");
 	})
