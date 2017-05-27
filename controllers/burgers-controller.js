8b228792f0cba528ac036750d00d12ddcbf40eb2
91integ25 <91integ25@gmail.com>
2017-05-27 10:38:18 -0400

    added some style

--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var db = require("../models")
-
+console.log(db.burger)
 router.get("/",function(req,res){
 	db.burger.findAll({}).then(function(result){
 		res.render("index",{burger:result});
@@ -16,10 +16,16 @@ router.put("/:id",function(req,res){
 	},}).then(function(result){
 		res.redirect("/");
 	})
+
+
 })
 router.post("/",function(req,res){
 	db.burger.create(req.body).then(function(){
 		res.redirect("/");
+	})
+	.catch(function(err){
+		console.log(err.erro)
+		res.send("Sorry you must enter a burger which you would like to make, Please go back and try again")
 	})
 });
 
